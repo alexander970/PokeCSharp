5306d49a38411fcd39b69903c4d8cb1c125d6ec2
alexander970 <50184502+alexander970@users.noreply.github.com>
2022-03-27 21:21:39 +0200

    Hello world

--- PokeCSharp.UI/Program.cs
+++ PokeCSharp.UI/Program.cs
@@ -12,7 +12,6 @@ namespace PokeCSharp.UI
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-            //helloworld
         }
     }
 }
