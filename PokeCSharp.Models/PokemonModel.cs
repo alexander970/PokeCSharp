9ad6332293c828a8184158aa1ddb9ba3f958d2b8
alexander970 <50184502+alexander970@users.noreply.github.com>
2022-03-27 22:54:50 +0200

    Create pokemon model

---
+++ PokeCSharp.Models/PokemonModel.cs
@@ -0,0 +1,15 @@
+﻿namespace PokeCSharp.Models
+{
+    public class PokemonModel
+    {
+        public int Id { get; set; }
+        public string? Identifier { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public int BaseExperience { get; set; }
+        public int Order { get; set; }
+        public bool IsDefault { get; set; }
+
+
+    }
+}
