93ae0c9f7912c99d49f6a0a0748761b4081d3c86
alexander970 <50184502+alexander970@users.noreply.github.com>
2022-03-29 20:10:15 +0200

    Pokemon initial model

--- PokeCSharp.Models/PokemonModel.cs
+++ PokeCSharp.Models/PokemonModel.cs
@@ -1,15 +1,29 @@
-﻿namespace PokeCSharp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokeCSharp.Models
 {
     public class PokemonModel
     {
-        public int Id { get; set; }
-        public string? Identifier { get; set; }
+        
+        [Required]
+        public int PokemonId { get; set; }
+
+        [Required]
+        public string PokemonName { get; set; } = string.Empty;
+
+        [Required]
         public int Height { get; set; }
+
+        [Required]
         public int Weight { get; set; }
+
+        [Required]
         public int BaseExperience { get; set; }
+
+        [Required]
         public int Order { get; set; }
+
+        [Required]
         public bool IsDefault { get; set; }
-
-
     }
 }
