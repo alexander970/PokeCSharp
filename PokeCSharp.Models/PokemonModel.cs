1488d53e95c00665962ebae34ae4491f65227be9
alexander970 <50184502+alexander970@users.noreply.github.com>
2022-03-29 20:24:22 +0200

    Create editor config file

--- PokeCSharp.Models/PokemonModel.cs
+++ PokeCSharp.Models/PokemonModel.cs
@@ -1,29 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace PokeCSharp.Models
+namespace PokeCSharp.Models;
+
+public class PokemonModel
 {
-    public class PokemonModel
-    {
-        
-        [Required]
-        public int PokemonId { get; set; }
+    
+    [Required]
+    public int PokemonId { get; set; }
 
-        [Required]
-        public string PokemonName { get; set; } = string.Empty;
+    [Required]
+    public string PokemonName { get; set; } = string.Empty;
 
-        [Required]
-        public int Height { get; set; }
+    [Required]
+    public int Height { get; set; }
 
-        [Required]
-        public int Weight { get; set; }
+    [Required]
+    public int Weight { get; set; }
 
-        [Required]
-        public int BaseExperience { get; set; }
+    [Required]
+    public int BaseExperience { get; set; }
 
-        [Required]
-        public int Order { get; set; }
+    [Required]
+    public int Order { get; set; }
 
-        [Required]
-        public bool IsDefault { get; set; }
-    }
+    [Required]
+    public bool IsDefault { get; set; }
 }
